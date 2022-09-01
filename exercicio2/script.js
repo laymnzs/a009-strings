472a0f6dc6d6618412439f8ef4a503496de4e1cd
layanemnzswok@gmail.com <layanemnzswork@gmail.com>
2022-09-01 18:49:50 -0300

    Exercícios Strings

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,8 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+let contagemEspacos = minhaString.trim();
+console.log(`Quantidade de caracteres: ${minhaString.length}`);
+console.log(contagemEspacos.length);
+
+//c)
+let replace = contagemEspacos.replace("________", "sticioso" )
+console.log(replace);
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,14 @@
 //Crie a const para a frase aqui
+//a)
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
+
+//b) 
+let troca = frase.replace("verde", "rosa").replace("azul", "laranja")
+console.log(troca)
+
+//c)
+console.log(`A nova frase possui a palavra "\ verde"\: ${frase.includes("verde")}\nA nova frase possui a palavra \"laranja\": ${frase.includes("laranja")}`)
+
+//extra
+const fraseMaiuscula = "mas não deixe o gato sair".toUpperCase()
+console.log(`Frase maiuscula: ${frase.replace(`mas não deixe o gato sair`, fraseMaiuscula)}`)
